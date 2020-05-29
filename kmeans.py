95de877b37e570d5e0c6a52beb8e46a28388bca2
tyler9937 <tyler9937@users.noreply.github.com>
2020-05-28 21:41:04 -0400

    added predict method

--- kmeans.py
+++ kmeans.py
@@ -1,20 +1,22 @@
-import numpy as np 
+import numpy as np
 import operator
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import random
 
-
-r = lambda: np.random.randint(1, 100)
 
 class Centroid:
-    def __init__(self,pos):
-        self.pos = pos 
+    def __init__(self, pos):
+        self.pos = pos
         self.points = []
         self.previous_points = []
         self.color = None
 
+
+r = lambda: np.random.randint(1, 100)
+
+
 class KMeans:
+
     def __init__(self, n_centroids=5):
         self.n_centroids = n_centroids
         self.centroids = []
@@ -27,14 +29,13 @@ class KMeans:
         colors = cm.rainbow(np.linspace(0, 1, len(self.centroids)))
         for i, c in enumerate(self.centroids):
             c.color = colors[i]
-        
+
     def sample_data(self, samples=50):
         self.X = [[r(), r()] for _ in range(samples)]
 
-
     def show(self):
         '''
-        displaysclustihg
+        displays clusting
         '''
         for i, c in enumerate(self.centroids):
             plt.scatter(c.pos[0], c.pos[1], marker='o', color=c.color, s=75)
@@ -42,14 +43,13 @@ class KMeans:
             y_cors = [y[1] for y in c.points]
 
             plt.scatter(x_cors, y_cors, marker='.', color=c.color)
-        
+
         title = 'K-means'
         plt.xlabel('X')
         plt.ylabel('Y')
         plt.title(title)
         plt.savefig('{}.png'.format(title))
         plt.show()
-
 
     def assign_centroid(self, x):
         '''
@@ -72,11 +72,9 @@ class KMeans:
             centroid.previous_points = centroid.points
             x_cor = [x[0] for x in centroid.points]
             y_cor = [y[1] for y in centroid.points]
-            try:
-                centroid.pos[0] = sum(x_cor)/len(x_cor)
-                centroid.pos[1] = sum(y_cor)/len(y_cor)
-            except:
-                pass
+
+            centroid.pos[0] = sum(x_cor)/len(x_cor)
+            centroid.pos[1] = sum(y_cor)/len(y_cor)
 
             if reset:
                 centroid.points = []
@@ -96,12 +94,16 @@ class KMeans:
             if len([c for c in self.centroids if c.points == c.previous_points]) == self.n_centroids:
                 fit = True
                 self._update_centroids(reset=False)
-            
+
             else:
                 self._update_centroids()
-            
+
             self.n_iters += 1
 
+    def predict(self, point):
+        closest = self.assign_centroid(point)
+        closest.points.append(point)
+        print(closest.pos)
 
 
 if __name__ == '__main__':
@@ -109,4 +111,5 @@ if __name__ == '__main__':
     km.sample_data()
     km.assign_centroid(km.X[0])
     km.fit()
+    km.predict([98, 98])
     km.show()
